59f8429f1f7b25061f90920a796faa27b0895974
Philipp Pracht <pp555@cam.ac.uk>
2023-03-11 12:40:52 +0000

    Add ALPB test singlepoint

--- testprog/main.f90
+++ testprog/main.f90
@@ -1,3 +1,21 @@
+!================================================================================!
+! This file is part of gfnff.
+!
+! Copyright (C) 2023 Philipp Pracht
+!
+! gfnff is free software: you can redistribute it and/or modify it under
+! the terms of the GNU Lesser General Public License as published by
+! the Free Software Foundation, either version 3 of the License, or
+! (at your option) any later version.
+!
+! gfnff is distributed in the hope that it will be useful,
+! but WITHOUT ANY WARRANTY; without even the implied warranty of
+! MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+! GNU Lesser General Public License for more details.
+!
+! You should have received a copy of the GNU Lesser General Public License
+! along with gfnff. If not, see <https://www.gnu.org/licenses/>.
+!================================================================================!
 program gfnff_main_tester
     use iso_fortran_env, only: wp=>real64,stdout=>output_unit
     use testmol
@@ -48,6 +66,11 @@ program gfnff_main_tester
 !=======================================================================================!
 !=======================================================================================!
 
+    write(*,*)
+    write(*,*) '================================================================='
+    write(*,*) '==================== GFN-FF SINGLEPOINT ========================='
+    write(*,*) '================================================================='
+  
 !>--- First, setup of parametrisation and topology into DAT
     write(*,*)
     write(*,*) 'Initializing GFN-FF'
@@ -64,6 +87,49 @@ program gfnff_main_tester
     write(*,*) 
     write(*,*) 'GFN-FF results'
     call print_gfnff_results(stdout, DAT%res, pr, allocated(DAT%solvation))
+
+    call DAT%deallocate()
+
+    write(*,*)
+    write(*,*) '=========================== END ================================='
+    write(*,*) '==================== GFN-FF SINGLEPOINT ========================='
+    write(*,*) '=========================== END ================================='
+
+
+!=======================================================================================!
+!=======================================================================================!
+!> USAGE WITH ALPB
+!=======================================================================================!
+!=======================================================================================!
+
+    write(*,*)
+    write(*,*) '================================================================='
+    write(*,*) '========= GFN-FF + ALPB implicit solvation SINGLEPOINT =========='
+    write(*,*) '================================================================='
+
+!>--- First, setup of parametrisation and topology into DAT
+    write(*,*)
+    write(*,*) 'Initializing GFN-FF + ALPB(water)'
+    DAT%solvent = 'h2o'
+    call gfnff_initialize(nat,at,xyz,DAT,'no file',.false.,.false.,ichrg=chrg,iostat=io)
+    write(*,*) 'Setup exit status:',io
+
+!>--- Then call to the singlepoint routine
+    write(*,*)
+    write(*,*) 'Performing GFN-FF singlepoint calculation'
+    call  gfnff_singlepoint(nat,at,xyz,DAT, energy,gradient, pr,iostat=io)
+    write(*,*) 'Singlepoint exit status:',io
+
+!>--- And printout of the results from DAT%res
+    write(*,*)
+    write(*,*) 'GFN-FF results'
+    call print_gfnff_results(stdout, DAT%res, pr, allocated(DAT%solvation))
+
+    write(*,*)
+    write(*,*) '========================== END =================================='
+    write(*,*) '========= GFN-FF + ALPB implicit solvation SINGLEPOINT =========='
+    write(*,*) '========================== END =================================='
+
 
 !=======================================================================================!
    deallocate(gradient)
